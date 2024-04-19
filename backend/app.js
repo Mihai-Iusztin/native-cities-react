2371cf4d2243155ba45b42e60049f73db4a464b5
Mihai-Iusztin <minai.iusztin@gmail.com>
2024-04-19 11:42:16 +0300

    add backend for data storage

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import express from 'express';
+const app = express();
+
+app.use(express.static('images'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Alow-Headers', 'Content-Type');
+
+  next();
+});
+
+app.get('/cities', async (req, res) => {
+  const fileContent = await fs.readFile('/data/cities.json');
+  const citiesData = JSON.parse(fileContent);
+  res.status(200).json({ cities: citiesData });
+});
+
+app.use('*', (req, res) => {
+  res.status(404).json({ message: '404 - Not Found' });
+});
+
+app.listen(3000, () => console.log('Running on port 3000!'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import SideMenu from './components/SideMenu';
 
 function App() {
-  return <SideMenu />;
+  return (
+    <main className="main-page">
+      <SideMenu />
+      <div className="content-page">
+        <header>
+          <img src="timisoara-header.jpg" alt="Beautiful Timisoara!" />
+          <h1>Amazing Cities</h1>
+        </header>
+      </div>
+    </main>
+  );
 }
 
 export default App;
